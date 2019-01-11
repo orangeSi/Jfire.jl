19c4c376648e2bc513e356a82413783342c3c0e8
orangeSi <sikaiwei@hk-software-install.hk.hpc>
2019-01-11 10:26:49 +0800

    update

--- doc/test.sh
+++ doc/test.sh
@@ -6,6 +6,7 @@ julia myth.jl hello --name world
 
 # call from multiple modules 
 julia myths.jl  myth1.hello1 --name world
+
 julia myths.jl  myth2.hello2 --name myth
 
 # call from single function
@@ -14,6 +15,7 @@ julia  func.jl wow
 
 # call from multiple functions
 julia  funcs.jl  myth_func1 well --greet 'nice day'
+
 julia  funcs.jl  myth_func2 well --greet 'nice day'
 
 echo test done
--- src/Jfire.jl
+++ src/Jfire.jl
@@ -7,9 +7,11 @@ export Fire
 
 using Dates
 
+
 function Fire(the_called::Union{Function, Module, Tuple};time::Bool=false, color::Symbol=:green)
-	printstyled("Jfire version 0.0.9\n",color=color)
-	printstyled("$(now()) ... start fire\n", color=color)	
+	io = IOContext(stdout, :color => true)
+	printstyled(io, "Jfire version 0.0.9\n",color=color)
+	printstyled(io, "$(now()) ... start fire\n", color=color)	
 	the_called_type = check_called_type(the_called)
 	need, kws, the_called = parse_args(ARGS, the_called_type, the_called)
 	
@@ -34,10 +36,10 @@ function Fire(the_called::Union{Function, Module, Tuple};time::Bool=false, color
 			call_function(the_called, need, kws)
 		end
 	else
-		error("sorry, not support the_called_type = $the_called_type for $the_called yet")
+		myexit("sorry, not support the_called_type = $the_called_type for $the_called yet")
 	end
 
-	printstyled("$(now()) ... end fire\n", color=color)	
+	printstyled(io, "$(now()) ... end fire\n", color=color)	
 
 end
 
@@ -52,77 +54,112 @@ function check_called_type(the_called::Union{Function, Module, Tuple})
 	elseif occursin(r"^Tuple{typeof", string(the_called_type))
 		the_called_type = "functions"
 	else
-		error("sorry, not support the_called_type = $the_called_type for $the_called yet")
+		myexit("sorry, not support the_called_type = $the_called_type for $the_called yet")
 	end
 	return the_called_type
 end
 
-function module_help(the_called::Module,args)
-	if length(args) < 1
-		error("sorry, you should specific a funciton name, not just a module name")
+function module_help(the_called::Module, args::Array{String}, exit_flag::Bool=true)
+	#if length(args) < 1
+	#	myexit("sorry, you should specific a funciton name, not just a module name")
+	#end
+	#if occursin(r"^-?-help" ,args[1])
+	#	myexit("you should first specific a module name then --help")
+	#elseif occursin(r"^-?-" ,args[1])
+	#	myexit("sorry, should start with function name, not $(args[1])")
+	#end
+	if length(args) >=1 && occur_help(args[1])
+		println("$the_called --help")
+		show_function_info(getfield(the_called, Symbol(args[1])))
 	end
-	if occursin(r"^-?-help" ,args[1])
-		error("you should first specific a function name then --help")
-	elseif occursin(r"^-?-" ,args[1])
-		error("sorry, should start with function name, not $(args[1])")
+	if length(args) >=2 && occur_help(args[2])
+		println("$the_called --help")
+		func = replace(args[1], r"^.*\."=>"")
+		show_function_info(getfield(the_called, Symbol(func)))
 	end
-	if length(args) >=2 && occursin(r"^-?-help" ,args[2])
-		help(getfield(the_called, Symbol(args[1])))
+end
+function occur_help(str::String)
+	if occursin(r"^-?-help$", str) || occursin(r"^-?-[Hh]$", str)
+		return true
+	else
+		return false
 	end
-	return 1
 end
 
-function parse_args(args, the_called_type, the_called)
+function parse_args(args::Array{String}, the_called_type, the_called)
 	if the_called_type == "module"
-		module_help(the_called, args)
+		module_help(the_called, args, true)
 		need, kws = parse_kws(args[2:end])
 		return need, kws, the_called
 	elseif the_called_type == "modules"
-		if length(args) == 0
-			error("error, you shold give Module and function name")
-		end
-		if ! occursin(r"\.", args[1])
-			error("format should like: module_name.fuction_name , not $(args[1]) for $the_called")
+		if length(args) >=1 && ! occursin(r"\.", args[1]) && ! occur_help(args[1])
+			myexit("format should like: module_name.fuction_name , not $(args[1]) for $the_called")
 		end
 		for m in the_called
 			the_type = typeof(m)
 			if the_type != Module
-				error("error, $m is not a Module name")
+				myexit("error, $m is not a Module name")
 			end
+			module_help(m, args, false)
 			if replace(string(m), r".*\."=>"") == replace(args[1], r"\..*"=>"")
-				module_help(m, args)
+				module_help(m, args, true)
 				need, kws = parse_kws(args[2:end])
 				return need, kws, m
 			end
 		end
-		error("error: cannot find module $(args[1])")
+		if ! occur_help(args[1])
+			myexit("error: cannot find module $(args[1]), only support $the_called")
+		else
+			exit()
+		end
 	elseif the_called_type == "function"
-		function_help(args, the_called)
+		function_help(args, the_called, true)
 		need, kws = parse_kws(args)
 		return need, kws, the_called
 	elseif the_called_type == "functions"
 		if length(args) == 0
-			error("error, you shold give function name")
+			myexit("error, you shold give function name")
 		end
 		for func in the_called
+			function_help(args, func, false)
 			if string(func) == args[1]
-				function_help(args, func)
+				function_help(args, func, true)
 				need, kws = parse_kws(args[2:end])
 				return need, kws, func
 			end
 		end
-		error("error, cannot find Function $(args[1]) in $the_called")
+		if ! occur_help(args[1])
+			myexit("error, cannot find Function $(args[1]) in $the_called")
+		else
+			exit()
+		end
 	else
-		error("sorry, not support $the_called_type yet, only Module or Funciton or tuple like (Module1,Module2)")
+		myexit("sorry, not support $the_called_type yet, only Module or Funciton or tuple like (Module1,Module2)")
 	end
 end
 
-function function_help(args, the_called::Function)
-	if length(args) >=1 && occursin(r"^-?-help" ,args[1])
-		help(the_called)
-	end
-
+function myexit(info::String)
+	println("\n$info\n")
+	exit()
+	#@error info
+	#@info info
+	#@warn info # https://docs.julialang.org/en/v1/stdlib/Logging/index.html
 end
+
+function function_help(args::Array{String}, the_called::Function, exit_flag::Bool=true)
+	if length(args) >=1 && occur_help(args[1])
+		show_function_info(the_called, exit_flag)
+	end
+end
+
+function show_function_info(func::Function, exit_flag::Bool)
+	dump(func)
+	println(methods(func))
+	if exit_flag
+		exit()
+	end
+end
+
 function get_help(the_called)
 	if typeof(the_called) == Module
 		println("here")
@@ -142,7 +179,7 @@ function get_help(the_called)
 end
 #methods(the_called.hello) # it works too !
 
-function parse_kws(args)
+function parse_kws(args::Array{String})
 	need = param_keys = param_values = []
 	flag = 0
 	# gather must need parameter
@@ -165,11 +202,11 @@ function parse_kws(args)
 	end
 	args = args[flag:end]
 	if ! (iseven(length(args)) || length(args) == 0)
-		error("sorry, parameter $args number should >=0 and is odd number")
+		myexit("sorry, parameter $args number should >=0 and is odd number")
 	end
 
 	# check parameter if start with --
-	[ occursin("-", args[i]) || error("sorry, $(args[i]) maybe is paramter, should start with --, like --$(args[i])") for i in 1:2:length(args)]
+	[ occursin("-", args[i]) || myexit("sorry, $(args[i]) maybe is paramter, should start with --, like --$(args[i])") for i in 1:2:length(args)]
 	
 	# gather keys of parameter
 	param_keys = [ Symbol(replace(args[i], '-' => "")) for i in 1:2:length(args)]
@@ -199,25 +236,10 @@ function convert_type(str::String)
 		the_type = getfield(Main, Symbol(m[1]))
 		return parse(the_type, m[2])
 	catch
-		error("error: $(m[1]) in $str is not support by julia Main Module")
+		myexit("error: $(m[1]) in $str is not support by julia Main Module")
 	end
 end
 
-function str2number(str::String)
-	if occursin(r"^[\d]+$", str)
-		return parse(Int, str)
-	elseif occursin(r"^[\d\.]+$", str)
-		if Sys.WORD_SIZE == 64
-			return parse(Float64, str)
-		elseif Sys.WORD_SIZE == 32
-			return parse(Float32, str)
-		else
-			error("error: Sys.WORD_SIZE is $(Sys.WORD_SIZE) is not supported, only support 32 o r 64")
-		end
-	else
-		return str
-	end
-end
 
 
 function call_module(the_called::Module, the_func, need::Tuple, kws::NamedTuple)
@@ -242,7 +264,7 @@ function call_module(the_called::Module, the_func, need::Tuple, kws::NamedTuple)
 	end
 
 	if flag == 1
-		error("sorry, not find function $the_func in $the_called")
+		myexit("sorry, not find function $the_func in $the_called")
 	end
 	# thanks to https://discourse.julialang.org/t/how-to-set-variable-to-key-of-keyword-arguments-of-function/18995
 	#
